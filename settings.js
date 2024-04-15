9fc8433dcf2cc5a9893f6c3794db85512945e71f
yvsc <108088938+yvsc@users.noreply.github.com>
2024-04-15 17:15:39 -0600

    Initial commit

---
+++ settings.js
@@ -0,0 +1,11 @@
+// Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
+// or a Google Sheets URL (File > Publish to the Web > CSV )
+const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS8eCLOLojyX3uiFFj8eRZv5ncFPy1CNu99bXzuCC643dz_jCmQfMAdrJQXpNoOavcaE5Ze3LcQQ06k/pub?gid=0&single=true&output=csv';
+
+// Map's initial center, and zoom level
+const mapCenter = [40.511, -107.210];
+const mapZoom = 9;
+
+// Marker icon height and width
+const iconHeight = 55;
+const iconWidth = 55;
