54c9213b175c715d5a1ca5327b9381e4fe763bd5
yvsc <108088938+yvsc@users.noreply.github.com>
2024-04-28 10:15:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -241,7 +241,7 @@ var initMap = function() {
 
   L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-	maxZoom: 1
+	maxZoom: 18
   }).addTo(map);
 
   loadData(dataLocation);
